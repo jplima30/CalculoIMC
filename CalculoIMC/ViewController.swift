9ce46095d31775f83b220c23a19fc8b4735ba2e4
João Silva <jpinfoagraria@gmail.com>
2019-08-05 20:03:32 -0300

    Implementação das funções e laços responsáveis pelo cálculo do IMC, assim como correção de bugs nas views e storyBoard

--- CalculoIMC/ViewController.swift
+++ CalculoIMC/ViewController.swift
@@ -14,14 +14,54 @@ class ViewController: UIViewController {
     @IBOutlet weak var tfHeight: UITextField!
     @IBOutlet weak var lbResult: UILabel!
     @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viResult: UIView!
+    
+    var imc: Double = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //code
+    }
 
-    @IBAction func Calculate(_ sender: Any) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        viResult.endEditing(true)
     }
     
+    @IBAction func Calculate(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!){
+            imc = weight / (height*height)
+            showResults()
+        }
+    }
+    func showResults(){
+        var result: String = ""
+        var image: String = ""
+        switch imc {
+        case 0..<16:
+            result = "Magreza"
+            image = "abaixo"
+        case 16..<18.5:
+            result = "Abaixo do peso"
+            image = "abaixo"
+        case 18.5..<25:
+            result = "Peso ideal"
+            image = "ideal"
+        case 25..<30:
+            result = "Sobrepeso"
+            image = "sobre"
+        default:
+            result = "Obesidade"
+            image = "obesidade"
+        }
+        lbResult.text = "\(Int(imc)): \(result)"
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false
+        view.endEditing(true)
+    }
 }
 
