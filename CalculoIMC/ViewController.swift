6eb54e0a542f5686bbbc090255022849f196be0d
João Silva <jpinfoagraria@gmail.com>
2019-08-05 01:30:05 -0300

    Criação das estruturas presentes no StoryBoard, implementação do auto layout e vinculação dos outlets

--- CalculoIMC/ViewController.swift
+++ CalculoIMC/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func Calculate(_ sender: Any) {
+    }
+    
 }
 
